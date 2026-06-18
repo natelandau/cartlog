aebfee6da1c5088779881cbfba13fb114c85ab45
Nathaniel Landau <github@natelandau.com>
2026-06-18 19:21:40 -0400

    test(web): drop unused pytest import

--- tests/web/test_units_display.py
+++ tests/web/test_units_display.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from decimal import Decimal
 
-import pytest
 from fastapi.testclient import TestClient
 
 from cartlog.web.units_display import format_normalized
